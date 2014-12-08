5f1d5e1259f357c1fc46921e9bdd52e3f119c5fe
Sourav Goswami <gosour@gmail.com>
2014-12-08 16:09:14 +0530

    RTP statistics added

--- OntheRoad/ip_udp_helloworld.c
+++ OntheRoad/ip_udp_helloworld.c
@@ -58,6 +58,7 @@
 /* The payload for the fake IP packet */
 #define FAKE_PAYLOAD "hello, ROHC world!"
  
+#define PACKET_COUNT 100
 /* return a random number every time it is called */
 static int gen_random_num(const struct rohc_comp *const comp,
                           void *const user_context)
@@ -68,24 +69,13 @@ static int gen_random_num(const struct rohc_comp *const comp,
 /* The main entry point of the program (arguments are not used) */
 int main(int argc, char **argv)
 {
+	/* Create the compressor first*/
    struct rohc_comp *compressor;  /* the ROHC compressor */
- 
-   /* the buffer that will contain the IPv4 packet to compress */
-   uint8_t ip_buffer[BUFFER_SIZE];
-   /* the packet that will contain the IPv4 packet to compress */
-   struct rohc_buf ip_packet = rohc_buf_init_empty(ip_buffer, BUFFER_SIZE);
-   /* the header of the IPv4 packet */
-   struct iphdr *ip_header;
- 
-   uint8_t rohc_buffer[BUFFER_SIZE];
-   /* the packet that will contain the resulting ROHC packet */
-   struct rohc_buf rohc_packet = rohc_buf_init_empty(rohc_buffer, BUFFER_SIZE);
- 
    rohc_status_t rohc_status;
    size_t i;
  
    /* print the purpose of the program on the console */
-   printf("This program will compress one single IPv4 packet\n");
+	printf("Analyzing multiple packet compression\n");
  
    /* initialize the random generator with the current time */
    srand(time(NULL));
@@ -103,7 +93,7 @@ int main(int argc, char **argv)
  
    /* enable the IP-only compression profile */
    printf("enable the IP-only compression profile\n");
-   if(!rohc_comp_enable_profile(compressor, ROHC_PROFILE_IP))
+   if(!rohc_comp_enable_profile(compressor, ROHC_PROFILE_UDP))
    {
       fprintf(stderr, "failed to enable the IP-only profile\n");
       /* cleanup compressor, then leave with an error code */
@@ -111,6 +101,21 @@ int main(int argc, char **argv)
       return 1;
    }
  
+ 	/* Creation of compressor ends here*/
+ 	
+ 	/* Generate multiple packets in this section*/
+ 	
+ 		/* the buffer that will contain the IPv4 packet to compress */
+   uint8_t ip_buffer[BUFFER_SIZE];
+   /* the packet that will contain the IPv4 packet to compress */
+   struct rohc_buf ip_packet = rohc_buf_init_empty(ip_buffer, BUFFER_SIZE);
+   /* the header of the IPv4 packet */
+   struct iphdr *ip_header;
+ 
+   uint8_t rohc_buffer[BUFFER_SIZE];
+   /* the packet that will contain the resulting ROHC packet */
+   struct rohc_buf rohc_packet = rohc_buf_init_empty(rohc_buffer, BUFFER_SIZE);
+
    /* create a fake IP packet for the purpose of this simple program */
    printf("build a fake IP packet\n");
    ip_header = (struct iphdr *) rohc_buf_data(ip_packet);
@@ -126,53 +131,26 @@ int main(int argc, char **argv)
    ip_header->check = 0x3fa9; /* checksum */
    ip_header->saddr = htonl(0x01020304); /* source address 1.2.3.4 */
    ip_header->daddr = htonl(0x05060708); /* destination addr. 5.6.7.8 */
-   
-   
+ 
    /* copy the payload just after the IP header */
    rohc_buf_append(&ip_packet, (uint8_t *) FAKE_PAYLOAD, strlen(FAKE_PAYLOAD));
  
-   /* dump the newly-created IP packet on terminal */
-   for(i = 0; i < ip_packet.len; i++)
-   {
-      printf("0x%02x ", rohc_buf_byte_at(ip_packet, i));
-      if(i != 0 && ((i + 1) % 8) == 0)
-      {
-         printf("\n");
-      }
-   }
-   if(i != 0 && (i % 8) != 0) /* be sure to go to the line */
-   {
-     printf("\n");
-   }
+
  
-   /* compress the fake IP packet */
-   printf("compress the fake IP packet\n");
    rohc_status = rohc_compress4(compressor, ip_packet, &rohc_packet);
    if(rohc_status != ROHC_STATUS_OK)
    {
-      fprintf(stderr, "compression of fake IP packet failed: %s (%d)\n",
-              rohc_strerror(rohc_status), rohc_status);
-      /* cleanup compressor, then leave with an error code */
-      rohc_comp_free(compressor);
-      return 1;
+	  fprintf(stderr, "compression of fake IP packet failed: %s (%d)\n",
+	          rohc_strerror(rohc_status), rohc_status);
+	  /* cleanup compressor, then leave with an error code */
+	  rohc_comp_free(compressor);
+	  return 1;
    }
  
-   /* dump the ROHC packet on terminal */
-   printf("ROHC packet resulting from the ROHC compression:\n");
-   for(i = 0; i < rohc_packet.len; i++)
-   {
-      printf("0x%02x ", rohc_buf_byte_at(rohc_packet, i));
-      if(i != 0 && ((i + 1) % 8) == 0)
-      {
-         printf("\n");
-      }
-   }
-   if(i != 0 && (i % 8) != 0) /* be sure to go to the line */
-   {
-      printf("\n");
-   }
- 
-   printf("destroy the ROHC decompressor\n");
+   printf("***Compressed Packet Length:\t%d\n",rohc_packet.len);
+	   
+		
+ 	}
    rohc_comp_free(compressor);
  
    /* leave the program with a success code */
